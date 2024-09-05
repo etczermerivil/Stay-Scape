97833277aaad9aaf6b3f7fcfd3f2123b0dc3c16e
Etczer <etczermerivil@gmail.com>
2024-09-05 14:25:40 -0400

    updated user model relationship to spots table

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,7 +4,11 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
+      User.hasMany(models.Spot, { foreignKey: 'ownerId' });
 
+      User.hasMany(models.Booking, { foreignKey: 'userId' });
+
+      User.hasMany(models.Review, { foreignKey: 'userId' });
     }
   };
 
