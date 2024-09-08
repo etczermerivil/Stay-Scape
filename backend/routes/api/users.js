a450dd0ba6632ddf2f69ddcda36110a18947f42d
JackieC-12 <tealkenn@gmail.com>
2024-09-08 17:27:23 -0500

    Added signup validation utils

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,19 +2,27 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation')
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Sign up
 router.post(
-    '/',
-    async (req, res) => {
-      const { email, password, username } = req.body;
+  '/',
+  validateSignup,
+  async (req, res) => {
+    const { firstName, lastName, email, username, password } = req.body;
+
+    try {
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
       };
@@ -25,6 +33,35 @@ router.post(
         user: safeUser
       });
     }
-  );
+
+    catch (error) {
+      if (error.name === 'SequelizeUniqueConstraintError') {
+        const errors = {}
+        error.errors.forEach((err) => {
+          errors[err.path] = `User with that ${err.path} already exists`
+        })
+
+        return res.status(500).json({
+          message: 'User already exists',
+          errors
+        })
+      }
+
+      else if (error.name === 'SequelizeValidationError') {
+        const errors = {}
+        error.errors.forEach((err) => {
+          errors[err.path] = err.message
+        })
+
+        return res.status(400).json({
+          message: 'Bad Request',
+          errors
+        })
+      }
+
+      next(error)
+    }
+  }
+);
 
 module.exports = router;
---
+++ backend/utils/validation.js
@@ -0,0 +1,46 @@
+const { validationResult } = require('express-validator');
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = {};
+    validationErrors.array().forEach((error) => {
+      errors[error.param] = error.msg;
+    });
+
+    // Respond directly without adding title or stack
+    return res.status(400).json({
+      message: 'Bad Request',
+      errors: errors
+    });
+  }
+  next();
+};
+
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
+
+
+module.exports = {
+  handleValidationErrors
+};
