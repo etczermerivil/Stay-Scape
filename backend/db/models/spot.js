c123a051e76ce4c5609ca69f99fdcf5899bae024
Etczer <etczermerivil@gmail.com>
2024-10-18 17:46:35 -0400

    updated backend spots model file/ fixed manage spots fecth

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -9,8 +9,7 @@ module.exports = (sequelize, DataTypes) => {
       Spot.belongsTo(models.User, { as: 'Owner', foreignKey: 'ownerId'});
       Spot.hasMany(models.SpotImage, {
         foreignKey: 'spotId',
-        onDelete: 'CASCADE',
-        as: 'SpotImages'
+        onDelete: 'CASCADE'
       });
       Spot.hasMany(models.Review, { foreignKey: 'spotId', onDelete: 'CASCADE' });
       Spot.hasMany(models.Booking, { foreignKey: 'spotId', onDelete: 'CASCADE' });
